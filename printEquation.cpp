001902504a8aac8d0a7e9909a323cd06f526d23e
nnikon <kolya.kasparov@gmail.com>
2023-08-24 18:39:50 +0300

    fix: change some expressions

--- printEquation.cpp
+++ printEquation.cpp
@@ -1,6 +1,7 @@
 #include "printEquation.h"
 void printFormattedEquation(const Coefficients coefficients)
 {
+    printf("\n");
 	// Careful govnokod
     // Handle cases where both a and b coefficients are zero.
     if (areSameDouble(coefficients.a, 0.0) && areSameDouble(coefficients.b, 0.0))
@@ -52,7 +53,7 @@ void printFormattedEquation(const Coefficients coefficients)
             printf("%g ", coefficients.c);
     }
 
-    printf("= 0\n");
+    printf("= 0\n\n");
 }
 
 void printAnswers(const Answers answers)
@@ -73,13 +74,13 @@ void printAnswers(const Answers answers)
         printf("x = %g\n\n", answers.x1);
         break;
     case NO_ROOTS:
-        printf("No ROOTs were found =[\n\n");
+        printf("No real roots were found =[\n\n");
         break;
     case INF_ROOTS:
-        printf("An infinite number of ROOTs were found!\n\n");
+        printf("An infinite number of roots were found!\n\n");
         break;
     default:
-        printf("Invalid amount of ROOTs\n\n"); // Never occurs.
+        printf("Invalid amount of roots\n\n"); // Never occurs.
         break;
     }
 }
